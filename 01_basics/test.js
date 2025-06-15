e216e7d6ad70986ef9ff3c78ac1dae0c1b6a2e75
Ram Kumar <itsyash23108@gmail.com>
2025-06-15 07:27:39 +0000

    changes

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,7 @@
-console.log("Radhe Krishna")
+const acc_id = 10343;
+let accemail = "radhaji@gmail.com"
+var accpswd = "Jaipur1212"
+accCity = "Jaipur"
+
+console.log(acc_id);
+console.table([acc_id, accemail, accpswd, accCity])
